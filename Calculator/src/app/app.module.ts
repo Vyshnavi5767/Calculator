15825cf2093872766ab6be8652b8bde908ab47d9
Sree Yalamuri <sreeyalamuri@Srees-MacBook-Air.local>
2017-09-05 15:47:58 -0700

    Create a working Calculator using Angular4 and css3

--- Calculator/src/app/app.module.ts
+++ Calculator/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 
+import "hammerjs";
+import { CalculatorComponent } from './calculator/calculator.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Calculator/src/app/calculator/calculator.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit, ViewChild, ContentChild } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+  Calculator:any;
+  CurrentValue:number;
+  previousValue:number = null;
+  computeMethod : string;
+  constructor() { }
+
+  ngOnInit() {
+  }
+  Clear(){
+    this.Calculator="";
+    this.CurrentValue = 0;
+    this.previousValue = 0;
+  }
+
+  OnCompute(){
+    switch(this.computeMethod){
+      case 'Division': 
+                 this.Calculator = this.previousValue / this.CurrentValue;
+                 console.log(this.Calculator);
+                break;
+      case  'Addition':
+              this.Calculator = this.previousValue + this.CurrentValue;
+              break;
+
+      case 'Subtraction':
+            this.Calculator = this.previousValue - this.CurrentValue;
+            break;
+      case 'Multiplication':
+            this.Calculator = this.previousValue * this.CurrentValue;
+            break;
+
+      default:
+         this.Calculator = 0;
+         break;
+        
+    }
+  }
+
+  onChangeValue(param:number){
+    this.Calculator= this.previousValue = this.Calculator ||this.CurrentValue;
+   this.Calculator =  this.CurrentValue = param;
+    console.log('The previous value is'+':'+' '+this.previousValue);
+    console.log('The Current Value is '+':'+' '+ this.CurrentValue);
+  }
+
+  Compute(Operation:string){
+    this.computeMethod = Operation;
+  }
+
+
+
+}
